c8957139d371743cd67985dc90f6a48528f4063b
Sourov Pal <85163889+sourovpal@users.noreply.github.com>
2022-09-18 20:29:42 +0600

    Update index.php

--- index.php
+++ index.php
@@ -7,3 +7,12 @@ $sell_price = 750;
 $discount = ((($price - $sell_price)*100)/$price);
 // 25% discount
 echo $discount;
+
+
+//==============================
+
+$price = 100;
+$discount = 20; // %
+$persent = ($price * $discount)/100;
+
+echo $persent; // 20%
